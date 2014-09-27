db4a8e2f6c7aaddd6676451a6fd1b10b864ceafc
Aleksandr Sher <aleksandrsher@gmail.com>
2014-09-27 10:00:50 +0000

    deck component

--- src/thehat/components.cljs
+++ src/thehat/components.cljs
@@ -6,11 +6,15 @@
             [cljs.core.async :as async :refer [<! >! chan close! put!]])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
+(defcomponent deck [data owner]
+  (render [_]
+    (dom/div {:class "deck" :data-deck-id (:id data)} (:name data))))
 
 (defcomponent game [data owner]
   (render [_]
     (dom/div
-     (dom/span (str "Decks count: " (count (:decks data)))))))
+      (dom/h3 "Select deck:")
+      (dom/span (str "Decks count: " (count (:decks data)))))))
 
 (defcomponent rules [data owner]
   (render [_]
