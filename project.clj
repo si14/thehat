53012db3cbbb9d31507f94d872205176cccf2be2
Aleksandr Sher <aleksandrsher@gmail.com>
2014-09-27 07:19:02 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject thehat "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2356"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [om "0.7.3"]]
+
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "thehat"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "thehat.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
