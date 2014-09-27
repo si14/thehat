869de2d5b79a507342c6db971fa0eb2768e27cc3
alwxndr <alwxndr@gmail.com>
2014-09-27 19:39:16 +0400

    animations

--- src/cljs/thehat/components/game_init.cljs
+++ src/cljs/thehat/components/game_init.cljs
@@ -2,15 +2,23 @@
   (:require [om-tools.dom :as dom :include-macros true]
             [om-tools.core :refer-macros [defcomponentk]]
             [thehat.helpers :refer [nbsp]]
-            [cljs.core.async :as async :refer [put!]]))
+            [cljs.core.async :as async :refer [put!]]
+            [dommy.utils :as utils]
+            [dommy.core :as dommy])
+  (:use-macros [dommy.macros :only [node sel sel1]]))
 
 (defn select-deck
   [id ch]
-  #(put! ch {:component :game-process :args {:deck-id id}}))
+  #(do
+     (dommy/remove-class! (sel1 (str "#deck_" id)) "flipInX")
+     (dommy/add-class! (sel1 (str "#deck_" id)) "bounce")
+     (dommy/listen! (sel1 (str "#deck_" id))
+                    :webkitAnimationEnd (fn [e] (put! ch {:component :game-process
+                                                          :args {:deck-id id}})))))
 
 (defn deck [{:keys [name id]} game-ch]
-  (dom/div {:class "pack" :on-click (select-deck id game-ch)}
-   (dom/div {:class "inside rotated"} nbsp)
+  (dom/div {:id (str "deck_" id) :class "pack animated flipInX" :on-click (select-deck id game-ch)}
+   (dom/div {:class "inside rotated"} "&nbsp;")
    (dom/div {:class "inside"}
     (dom/div {:class "word"} name))))
 
