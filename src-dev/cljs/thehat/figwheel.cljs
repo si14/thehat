76caab18206412038ea2d1717ccac853058e0b38
Aleksandr Sher <aleksandrsher@gmail.com>
2014-09-27 14:39:46 +0000

    fix

--- src-dev/cljs/thehat/figwheel.cljs
+++ src-dev/cljs/thehat/figwheel.cljs
@@ -1,3 +1,13 @@
-(ns thehat.figwheel)
+(ns thehat.figwheel
+  (:require 
+    [figwheel.client :as fw :include-macros true]
+    [cljs.core.async :as async :refer [put!]]
+    [thehat.components :refer [game]]
+    [thehat.core :refer [app-state route-ch]]))
 
 (enable-console-print!)
+
+(fw/watch-and-reload
+ :jsload-callback  (fn []
+                     (reset! app-state @app-state)
+                     (put! route-ch {:component game})))
