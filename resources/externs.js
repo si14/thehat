6d706584e1e2aa244eace2e561186c763e8e6d01
Dmitry Groshev <lambdadmitry@gmail.com>
2014-09-29 03:37:52 +0400

    trying to fix google sharing

--- resources/externs.js
+++ resources/externs.js
@@ -2,3 +2,7 @@ var GeoPattern = {};
 GeoPattern.generate = function(name) {};
 GeoPattern.generate.toDataUrl = function(name) {};
 GeoPattern.generate.toDataUri = function(name) {};
+
+var gapi = {};
+gapi.plus = {};
+gapi.plus.go = function() {};
