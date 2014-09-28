99d2529bbb1fbfd4f6ac0d7d4c538128fd96a9f9
Dmitry Groshev <lambdadmitry@gmail.com>
2014-09-28 17:24:14 +0400

    sound notification kinda works

--- src/cljs/thehat/sound.cljs
+++ src/cljs/thehat/sound.cljs
@@ -49,80 +49,46 @@
         osc2 (.createOscillator ctx)
         gain1 (.createGain ctx)
         gain2 (.createGain ctx)
-        filter1 (.createBiquadFilter ctx)
+
         attack 15
-        plateau 200
-        decay 50
+        plateau 150
+        decay 30
         pulse-length (+ attack plateau decay)
+        final-pulse-length (* pulse-length 2)
         overall-length 5000
-        exponent 0.75
-        pattern (->> overall-length
-                     (iterate (partial * exponent))
-                     (take-while (fn [x] (> (- (/ x exponent) x)
-                                            (* pulse-length exponent))))
-                     (map #(- overall-length %))
-                     (map #(/ % 1000)))
-        current-time (.-currentTime ctx)]
+        exponent 0.5
 
-    (prn pattern)
-
-    ;; (.connect gain1 dest)
-    ;; (.setValueAtTime (-> gain1 .-gain) 0 (.-currentTime ctx))
-    ;; (.linearRampToValueAtTime (-> gain1 .-gain) 1 (+ (.-currentTime ctx) (/ attack 1000)))
-    ;; (.linearRampToValueAtTime (-> gain1 .-gain) 0 (+ (.-currentTime ctx) (/ decay 1000)))
-
+        current-time (.-currentTime ctx)
+        stop-time (+ current-time (/ overall-length 1000))]
 
     (.connect gain1 dest)
-    (.setValueAtTime (-> gain1 .-gain) 0 current-time)
-    (doseq [start (butlast pattern)
-            :let [pulse-ramp-up-start (+ current-time start)
-                  pulse-plateau-start (+ pulse-ramp-up-start (/ attack 1000))
-                  pulse-ramp-down-start (+ pulse-plateau-start (/ plateau 1000))
-                  pulse-end (+ pulse-ramp-down-start (/ decay 1000))]]
-      (prn pulse-ramp-up-start pulse-plateau-start
-           pulse-ramp-down-start pulse-end)
+    (.setValueAtTime (-> gain1 .-gain) 0 0)
+
+    (loop [pulse-ramp-up-start current-time
+           pause 1.25]
       (.setValueAtTime (-> gain1 .-gain) 0 pulse-ramp-up-start)
-      (.linearRampToValueAtTime (-> gain1 .-gain) 1 pulse-plateau-start)
-      (.setValueAtTime (-> gain1 .-gain) 1 pulse-ramp-down-start)
-      (.linearRampToValueAtTime (-> gain1 .-gain) 0 pulse-end))
+      (if (<= (+ pulse-ramp-up-start
+                 (/ pulse-length 1000)
+                 pause)
+             stop-time)
+        (let [pulse-plateau-start (+ pulse-ramp-up-start (/ attack 1000))
+              pulse-ramp-down-start (+ pulse-plateau-start (/ plateau 1000))
+              pulse-end (+ pulse-ramp-down-start (/ decay 1000))]
+          (prn pulse-ramp-up-start pulse-end)
+          (.linearRampToValueAtTime (-> gain1 .-gain) 0.5 pulse-plateau-start)
+          (.setValueAtTime (-> gain1 .-gain) 0.5 pulse-ramp-down-start)
+          (.linearRampToValueAtTime (-> gain1 .-gain) 0 pulse-end)
+          (recur (+ pulse-end pause) (* pause exponent)))
+        (do
+          (.linearRampToValueAtTime (-> gain1 .-gain) 1 (+ pulse-ramp-up-start (/ attack 1000)))
+          (.setValueAtTime (-> gain1 .-gain) 1 (+ stop-time (/ final-pulse-length 1000)))
+          (.linearRampToValueAtTime (-> gain1 .-gain) 0 (+ stop-time
+                                                           (/ final-pulse-length 1000)
+                                                           (/ decay 1000))))))
 
-    (let [last-pulse-start (+ current-time (last pattern))
-          pulse-plateau-start (+ last-pulse-start (/ attack 1000))]
-      (prn last-pulse-start pulse-plateau-start (+ current-time (/ overall-length 1000)))
-      (.setValueAtTime (-> gain1 .-gain) 0 last-pulse-start)
-      (.linearRampToValueAtTime (-> gain1 .-gain) 1 pulse-plateau-start))
-
-    ;; (.linearRampToValueAtTime (-> gain1 .-gain) 1 (+ (.-currentTime ctx) 1))
-    ;; (.linearRampToValueAtTime (-> gain1 .-gain) 0 (+ (.-currentTime ctx) 2))
-
-    (set! (-> osc1 .-frequency .-value) 500)
+    (set! (-> osc1 .-frequency .-value) 600)
     (set! (.-type osc1) "sine")
     (.connect osc1 gain1)
     (.start osc1 current-time)
-    (.stop osc1 (+ current-time (/ overall-length 1000)))
-
-    (set! (-> osc2 .-frequency .-value) 600)
-    (set! (.-type osc2) "sine")
-    (.connect osc2 dest)
-    (.start osc2 (+ (.-currentTime ctx) (/ overall-length 1000)))
-    (.stop osc2 (+ (.-currentTime ctx) (/ overall-length 1000)  (/ decay 1000)))
-
-
-    ;; (set! (-> osc1 .-frequency .-value) 400)
-    ;; (set! (.-type osc1) "sine")
-    ;; (.connect osc1 gain1)
-    ;; (.start osc1 0)
-
-    ;; (set! (-> gain .-gain .-value) 100)
-    ;; (.connect gain (.-frequency osc1))
-
-    ;; (set! (-> osc2 .-frequency .-value) 1)
-    ;; (.connect osc2 gain)
-    ;; (.start osc2 0)
-
-    (js/setTimeout (fn [] (.stop osc1 0) (.disconnect osc1 dest) (prn "stop")) 6000))
-  )
-
-(fw/watch-and-reload)
-
-(make-sound)
+    (.stop osc1 (+ stop-time (/ final-pulse-length 1000)
+                   (/ decay 1000)))))
