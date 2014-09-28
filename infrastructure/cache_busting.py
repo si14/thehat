7a3567cc740ccb08cdcea17b2fc262324e1e6d82
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2014-09-28 20:43:33 +0200

    cache busting accepts html path and arbitrary number of js/css paths

--- infrastructure/cache_busting.py
+++ infrastructure/cache_busting.py
@@ -1,30 +1,61 @@
-import os
 from bs4 import BeautifulSoup
 import hashlib
+from argparse import ArgumentParser
 
-script_path = os.path.abspath(__file__)
-project_path = os.path.dirname(os.path.dirname(script_path))
-js_path = os.path.join(project_path, "resources", "public",
-                       "thehat.js")
-index_path = os.path.join(project_path, "resources", "public",
-                          "index.html")
+parser = ArgumentParser()
+parser.add_argument("index")
+parser.add_argument("resources", nargs="+")
+opts = parser.parse_args()
 
-js_hash = hashlib.sha224(open(js_path).read()).hexdigest()
-soup = BeautifulSoup(open(index_path).read())
+soup = BeautifulSoup(open(opts.index).read())
 scripts = soup.find_all("script")
-for script in scripts:
-    if script["src"].startswith("js/compiled/thehat.js"):
-        break
-else:
-    raise Exception("script is not found in index!")
+styles = soup.find_all("link")
 
-script_src_split = script["src"].split("?hash=")
 
-if len(script_src_split) == 1:
-    script_src_split.append(js_hash)
-elif not script_src_split[-1].endswith(js_hash):
-    script_src_split[-1] = js_hash
+def process_js(resource_name, hash):
+    for script in scripts:
+        if resource_name in script.get("src", ""):
+            break
+    else:
+        raise Exception("js %s is not found in index!" % resource_name)
 
-script["src"] = "?hash=".join(script_src_split)
+    script_src_split = script["src"].split("?hash=")
+
+    if len(script_src_split) == 1:
+        script_src_split.append(hash)
+    elif not script_src_split[-1].endswith(hash):
+        script_src_split[-1] = hash
+
+    script["src"] = "?hash=".join(script_src_split)
+
+
+def process_css(resouce_name, hash):
+    for style in styles:
+        if resource_name in style["href"]:
+            break
+    else:
+        raise Exception("css %s is not found in index!" % resource_name)
+
+    style_href_split = style["href"].split("?hash=")
+
+    if len(style_href_split) == 1:
+        style_href_split.append(hash)
+    elif not style_href_split[-1].endswith(hash):
+        style_href_split[-1] = hash
+
+    style["href"] = "?hash=".join(style_href_split)
+
+
+for resource in opts.resources:
+    hash = hashlib.sha224(open(resource).read()).hexdigest()
+    # assuming all the files have different names
+    resource_name = resource.split("/")[-1]
+
+    if resource_name.endswith(".js"):
+        process_js(resource_name, hash)
+    elif resource_name.endswith(".css"):
+        process_css(resource_name, hash)
+
+
 html = soup.prettify(soup.original_encoding)
-open(index_path, "w+").write(html)
+open(opts.index, "w+").write(html)
