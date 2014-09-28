8b493e1cb91f97393761f9e34ddc2cf517d12c7f
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2014-09-28 18:41:38 +0200

    nlp deps moved to dev profile

--- project.clj
+++ project.clj
@@ -6,11 +6,6 @@
                  [org.clojure/clojurescript "0.0-2356"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.7.3"]
-                 [org.clojure/data.json "0.2.5"]
-                 [aysylu/loom "0.5.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1"
-                  :classifier "models"]
                  [prismatic/om-tools "0.3.3" :exclusions [org.clojure/clojure]]
                  [secretary "1.2.0"]
                  [prismatic/dommy "0.1.3"]]
@@ -19,7 +14,12 @@
 
   :source-paths ["src/clj"]
 
-  :profiles {:dev {:dependencies [[figwheel "0.1.4-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[figwheel "0.1.4-SNAPSHOT"]
+                                  [aysylu/loom "0.5.0"]
+                                  [org.clojure/data.json "0.2.5"]
+                                  [edu.stanford.nlp/stanford-corenlp "3.3.1"]
+                                  [edu.stanford.nlp/stanford-corenlp "3.3.1"
+                                   :classifier "models"]]
                    :figwheel {:css-dirs ["resources/public/css"]}
                    :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]}}
 
