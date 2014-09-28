c925f7b1c0cb8c0d0bee5520433e6dc580cacb9f
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2014-09-28 18:15:52 +0200

    real cards added to game

--- src/cljs/thehat/cards.cljs
+++ src/cljs/thehat/cards.cljs
@@ -1,13 +1,35 @@
-(ns thehat.cards)
+(ns thehat.cards
+  (:require [thehat.words :as words]))
 
 (declare decks)
 
 (defn get-decks [] (map #(assoc % :words (-> % :words shuffle)) decks))
 
-(def decks [{:id 1 :name "All" :words ["hello" "world"]}
+(def decks [{:id 1 :name "All" :words ["hello" "world" "1" "2" "3" "4" "5" "6"]}
             {:id 2 :name "Animals" :words ["why" "guys" "you"]}
             {:id 3 :name "Temp" :words ["so" "long" "and" "longer"]}
             {:id 4 :name "Food" :words ["cola" "russian" "bear" "beer"]}
             {:id 5 :name "Seas" :words ["cola" "russian" "bear" "beer"]}
-            {:id 6 :name "Drinks!" :words ["cola" "russian" "bear" "beer"]}
-            ])
+            {:id 6 :name "Drinks!" :words ["cola" "russian" "bear" "beer"]}])
+
+
+
+
+
+
+
+
+
+(def decks [{:name "Random"
+             :words (concat words/cs words/common words/animals
+                            words/clothes words/art words/food)}
+            {:name "Computer Science"
+             :words words/cs}
+            {:name "Common"
+             :words words/common}
+            {:name "Animals"
+             :words words/animals}
+            {:name "Clothes"
+             :words words/clothes}
+            {:name "Food"
+             :words words/food}])
