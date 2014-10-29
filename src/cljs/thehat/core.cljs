8f098c55b3800f2db5376f678dd9465d15d7c3b2
Dmitry Groshev <lambdadmitry@gmail.com>
2014-10-29 20:15:22 +0300

    fix notification, seems to work at least in narrow chrome

--- src/cljs/thehat/core.cljs
+++ src/cljs/thehat/core.cljs
@@ -14,10 +14,11 @@
 
 (def config
   {
-   ;; :round-duration 30
-   :round-duration 10
-   ;; :max-score 42
-   :max-score 5
+   :round-duration 30
+   ;; :round-duration 10
+   :max-score 24
+   ;; :max-score 5
+   :sound-warning-percent 0.67 ;; should be in sync with progressRunDown in CSS
    :team-names ["Blue" "Green"]})
 
 ;;
@@ -25,26 +26,14 @@
 ;;
 
 (defonce current-screen (atom :deck-chooser))
-(defonce current-time (atom (:round-duration config)))
+(defonce current-time-left (atom (:round-duration config)))
 (defonce current-deck (atom nil))
 (defonce current-scores (atom [0 0]))
 (defonce current-team (atom 0))
+(defonce current-round (atom 0))
 
 (defonce interaction-chan (chan))
 (defonce timer-stop-chan (chan))
-
-;;
-;; Utils
-;;
-
-;; FIXME(Dmitry): this shit sucks, it's better to remove it somehow
-(defn listen-animation-end! [el handler]
-  ;; NOTE(Dmitry): to my surprise, at least in Chrome case matters
-  (doseq [event-name ["animationend"
-                      "webkitAnimationEnd"
-                      "MSAnimationEnd"
-                      "oanimationend"]]
-    (dommy/listen-once! el event-name handler)))
 
 ;;
 ;; Components
@@ -76,7 +65,7 @@
      [:span.desktop "Click"]
      " anywhere to start the game"]]])
 
-(defn round-buttons-one []
+(defn round-buttons [both?]
   (let [team @current-team]
     [:div.buttons
      [:div.small nbsp]
@@ -85,32 +74,27 @@
                         {:type :wrong-card
                          :team team})}]
      [:span nbsp]
-     [(case @current-team
-        0 :span.icon.icon-checkmark.bt-right-team-0
-        1 :span.icon.icon-checkmark.bt-right-team-1)
-      {:on-click #(put! interaction-chan
-                        {:type :right-card
-                         :team team})}]]))
-
-(defn round-buttons-both []
-  (let [team @current-team]
-    [:div.buttons
-     ;; FIXME(Dmitry): next 5 lines are identical in both components
-     [:div.small nbsp]
-     [:span.icon.icon-cancel-2.bt-wrong
-      {:on-click #(put! interaction-chan
-                        {:type :wrong-card
-                         :team team})}]
-     [:span nbsp]
-     [:span.icon.icon-checkmark.bt-right-team-0
-      {:on-click #(put! interaction-chan
-                        {:type :right-card
-                         :team 0})}]
-     [:span nbsp]
-     [:span.icon.icon-checkmark.bt-right-team-1
-      {:on-click #(put! interaction-chan
-                        {:type :right-card
-                         :team 1})}]]))
+     (if both?
+       (seq
+        [^{:key :team-0}
+         [:span.icon.icon-checkmark.bt-right-team-0
+          {:on-click #(put! interaction-chan
+                            {:type :right-card
+                             :team 0})}]
+         ^{:key :nbsp-0}
+         [:span nbsp]
+         ^{:key :team-1}
+         [:span.icon.icon-checkmark.bt-right-team-1
+          {:on-click #(put! interaction-chan
+                            {:type :right-card
+                             :team 1})}]])
+       ^{:key (case team 0 :team-0 1 :team-1)}
+       [(case team
+          0 :span.icon.icon-checkmark.bt-right-team-0
+          1 :span.icon.icon-checkmark.bt-right-team-1)
+        {:on-click #(put! interaction-chan
+                          {:type :right-card
+                           :team team})}])]))
 
 (defn team [active? arrow-class score-class score]
   (let [offset (str (- (* 95 (/ score (:max-score config))) 95) "%")
@@ -129,18 +113,24 @@
      [team (= active-team 0) :arrow-0 :score-0 score-0]
      [team (= active-team 1) :arrow-1 :score-1 score-1]]))
 
-(defn round []
-  (let [time @current-time
+(defn round-plain []
+  (let [time @current-time-left
         deck @current-deck
+        round-n @current-round
         progress-width (str (- 100 (* 100 (/ time (:round-duration config))))
                             "%")
         progress-translation (str "translate3d(-" progress-width ",0,0)")
         word (first (:words deck))]
     [:div.game
+     ;; NOTE(Dmitry): prevent reuse of this DOM element between rounds
+     ^{:key round-n}
      [:div.time
       (if (pos? time)
         [:div.progress.active
-         {:style {:transform progress-translation}}
+         {:style #_{:transform progress-translation}
+          {:-webkit-animation-duration (str (:round-duration config) "s")
+           :animation-duration (str (:round-duration config) "s")}
+          }
          [:span.label (int time)]]
         nbsp)]
      [:div#rotated-card.card-inner.card-rotated
@@ -153,10 +143,15 @@
       [:div#current-card.card-inner.keyframe-animated
        [:div.word word
         [:div.buttons
-         (if (pos? time)
-           [round-buttons-one]
-           [round-buttons-both])]]]]
+         [round-buttons (not (pos? time))]]]]]
      [teams]]))
+
+(def round
+  (with-meta round-plain
+    {:component-did-mount (fn [_] (notification/start
+                                   (:round-duration config)
+                                   (:sound-warning-percent config)))
+     :component-will-unmount (fn [_] (notification/stop))}))
 
 (defn interlude []
   [:div
@@ -193,8 +188,6 @@
    :interlude interlude
    :final final})
 
-
-
 (defn root []
   (let [screen @current-screen]
      ^{:key screen}
@@ -208,43 +201,40 @@
 
 (defmethod interaction [:deck-chooser :deck-click]
   [{:keys [deck-id]}]
-  (let [el (sel1 (str "#deck_" deck-id))
-        deck (get decks deck-id)
-        shuffled-deck (update-in deck [:words] shuffle)
-        animation-end-handler
-        (fn [e]
-          (reset! current-deck shuffled-deck)
-          (reset! current-screen :prelude))]
-    (dommy/remove-class! el "flipInX")
-    #_(dommy/add-class! el "bounce")
-    (animation-end-handler)
-    #_(listen-animation-end! el animation-end-handler)))
+  (let [deck (get decks deck-id)
+        shuffled-deck (update-in deck [:words] shuffle)]
+    (reset! current-deck shuffled-deck)
+    (reset! current-screen :prelude)))
 
 (defn start-ticker []
-  (go-loop [time (:round-duration config)]
-    ;; FIXME(Dmitry): it can be done better to account for time drift
-    ;;                (timeout based on actual time)
-    (let [to (async/timeout 1000)
-          [_ ch] (alts! [timer-stop-chan to])
-          new-time (dec time)]
-      (when (and (= ch to)
-                 (>= new-time 0))
-        (do (reset! current-time new-time)
-            (recur new-time))))))
+  (let [max-time (:round-duration config)
+        start-ts (js/Date.now)]
+    (go-loop [time 0]
+      (let [new-time (inc time)
+            to-sleep (- (+ start-ts (* 1000 new-time))
+                        (js/Date.now))
+            to (async/timeout (max to-sleep 0))
+            [_ ch] (alts! [timer-stop-chan to])]
+        (when (and (= ch to)
+                   (<= new-time max-time))
+          (do (reset! current-time-left (- max-time new-time))
+              (recur new-time)))))))
 
 (defmethod interaction [:prelude :prelude-click]
   [_]
   (reset! current-team (if (> (js/Math.random) 0.5) 0 1))
   (reset! current-scores [0 0])
-  (reset! current-time (:round-duration config))
+  (reset! current-time-left (:round-duration config))
+  (reset! current-round 0)
   (reset! current-screen :round)
+  #_(notification/start (:round-duration config))
   (start-ticker))
 
 (defmethod interaction [:round :wrong-card]
   [{:keys [team]}]
   (dommy/add-class! (sel1 :#current-card) :wrong)
   (swap! current-scores update-in [team] #(if (pos? %) (dec %) %))
-  (if (pos? @current-time)
+  (if (pos? @current-time-left)
     (swap! current-deck update-in [:words] rest)
     (reset! current-screen :interlude)))
 
@@ -255,7 +245,7 @@
     (if (or (>= (get scores team) (:max-score config))
             (<= (count (:words @current-deck)) 0))
       (reset! current-screen :final)
-      (if (pos? @current-time)
+      (if (pos? @current-time-left)
         (swap! current-deck update-in [:words] rest)
         (reset! current-screen :interlude)))))
 
@@ -263,7 +253,8 @@
   [{:keys [team]}]
   (swap! current-team #(- 1 %))
   (swap! current-deck update-in [:words] rest)
-  (reset! current-time (:round-duration config))
+  (swap! current-round inc)
+  (reset! current-time-left (:round-duration config))
   (reset! current-screen :round)
   (start-ticker))
 
--- src/cljs/thehat/notification.cljs
+++ src/cljs/thehat/notification.cljs
@@ -1,7 +1,7 @@
 (ns thehat.notification
   (:require
    [figwheel.client :as fw :include-macros true]
-   [dommy.core :as dommy :refer-macros [node sel sel1]]))
+   [dommy.core :as dommy :refer-macros [sel sel1]]))
 
 (enable-console-print!)
 
@@ -34,12 +34,12 @@
 (defonce ctx (create-context))
 
 (def osc-atom (atom nil))
+(def gain-atom (atom nil))
 
 (defn make-sound [overall-length]
   (let [dest (.-destination ctx)
         osc1 (.createOscillator ctx)
         gain1 (.createGain ctx)
-        gain2 (.createGain ctx)
 
         attack 15
         plateau 150
@@ -52,12 +52,13 @@
         stop-time (+ current-time (/ overall-length 1000))]
 
     (reset! osc-atom osc1)
+    (reset! gain-atom gain1)
 
     (.connect gain1 dest)
     (.setValueAtTime (-> gain1 .-gain) 0 0)
 
     (loop [pulse-ramp-up-start current-time
-           pause 1.25]
+           pause 2.5]
       (.setValueAtTime (-> gain1 .-gain) 0 pulse-ramp-up-start)
       (if (<= (+ pulse-ramp-up-start
                  (/ pulse-length 1000)
@@ -81,7 +82,8 @@
     (set! (.-type osc1) "sine")
     (.connect osc1 gain1)
     (.start osc1 current-time)
-    (.stop osc1 (+ stop-time (/ final-pulse-length 1000)
+    (.stop osc1 (+ stop-time
+                   (/ final-pulse-length 1000)
                    (/ decay 1000)))))
 
 (defn vibrate [length]
@@ -90,17 +92,22 @@
      (.-webkitVibrate js/navigator) (.webkitVibrate js/navigator 1000)
      (.-mozVibrate js/navigator) (.mozVibrate js/navigator 1000)))
 
-(defn start-notifying []
-  (let [overall-length 5000]
+(defn start
+  "Takes round length in seconds and percent at which
+   notification should start"
+  [round-length warning-percent]
+  (let [round-length-msec (* 1000 round-length)]
     (when ctx
-      (make-sound overall-length))
-    ;; FIXME(Dmitry): hack for avoiding vibration if notification is cancelled
-    (js/setTimeout #(when @osc-atom (vibrate 1000)) overall-length)))
+      (js/setTimeout #(make-sound (* round-length-msec (- 1 warning-percent)))
+                     (* round-length-msec warning-percent)))
+    ;; NOTE(Dmitry): hack for avoiding vibration if notification is cancelled
+    (js/setTimeout #(when @osc-atom (vibrate 1000)) round-length-msec)))
 
-(defn stop-notifying []
-  #_(when @osc-atom
-    ;; FIXME(Dmitry): this causes a click
-    (.stop @osc-atom 0)
+(defn stop []
+  (when @osc-atom
+    (let [current-time (.-currentTime ctx)]
+      (.linearRampToValueAtTime (-> @gain-atom .-gain) 0 (+ current-time 0.1))
+      (.stop @osc-atom (+ current-time 0.1)))
     (reset! osc-atom nil)))
 
 (defn unlock-notification []
